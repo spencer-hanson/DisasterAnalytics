eb5ff07d240c2a3eccf96f96af35ea88743c111a
Sam Reed <samdreed21@gmail.com>
2018-04-01 17:58:22 -0600

    Able to specify date range for queries

--- twitter_api_calls.py
+++ twitter_api_calls.py
@@ -1,19 +1,22 @@
 from searchtweets import ResultStream, gen_rule_payload, load_credentials
 from searchtweets import collect_results
-from tweet_parser.tweet import Tweet
-from tweet_parser.tweet_parser_errors import NotATweetError
-import fileinput
-import json
 
 #  Function call to load credentials
 enterprise_search_args = load_credentials("credentials.twitter_keys.yaml",
                                           yaml_key="search_tweets_enterprise",
                                           env_overwrite=False)
 
-rule = gen_rule_payload("Earthquake", results_per_call=100)  # testing with a sandbox account
+#  Specify timeperiod  year(yyyy) - month(mm) - day(dd) - time(hhmm)
+fromdate = "201207220000"
+todate = "201208220000"
+
+rule = gen_rule_payload("Earthquake", from_date=fromdate, to_date=todate, results_per_call=100)
 print(rule)
 
 tweets = collect_results(rule,
                          max_results=100,
                          result_stream_args=enterprise_search_args)  # change this if you need to
-[print(tweet.all_text, end='\n\n') for tweet in tweets[0:10]]
+
+[print(tweet.all_text, '\n', "Time: ", tweet.created_at_datetime, '\n', "Source:", tweet.generator.get("name"), '\n')
+ for tweet in tweets[0:10]]
+
