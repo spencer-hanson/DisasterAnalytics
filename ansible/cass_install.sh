183d5b679e1909cdd76daf02499a2d26f3a8962e
Spencer Hanson <spencer.hanson@swimlane.com>
2018-04-06 15:35:50 -0600

    Added cassandra deploy script and dependencies

---
+++ ansible/cass_install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "deb http://www.apache.org/dist/cassandra/debian 36x main" | sudo tee -a /etc/apt/sources.list.d/cassandra.sources.list
+curl https://www.apache.org/dist/cassandra/KEYS | sudo apt-key add -
+sudo apt-get -y update
+sudo apt-get -y install cassandra
+
+wget -O jre.tar.gz https://www.dropbox.com/s/nr59ymvy8dbcywg/jre-8u152-linux-x64.tar.gz?dl=0
+sudo mkdir /opt/jre
+sudo tar -zxf jre.tar.gz -C /opt/jre
+
+sudo update-alternatives --install /usr/bin/java java /opt/jre/jre1.8.0_152/bin/java 2000
