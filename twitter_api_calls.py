9d51493c7e3834ad463e2be1909b8f9ef594e8db
Spencer Hanson <spencer.hanson@swimlane.com>
2018-04-10 18:42:52 -0600

    Fixed cassandra connection

--- twitter_api_calls.py
+++ twitter_api_calls.py
@@ -2,23 +2,16 @@ from searchtweets import ResultStream, gen_rule_payload, load_credentials
 from searchtweets import collect_results
 from cassandra.cluster import Cluster
 from cassandra.query import SimpleStatement
+import uuid
 
 cluster = Cluster()
-session = cluster.connect('disasteranalytics')
-
-
-
-
-query = SimpleStatement("INSERT INTO users (Text, Time, Source, Coordinates) VALUES (%s, %s, %s, %s)")
-
-
+session = cluster.connect("disasteranalytics")
 
 
 #  Function call to load credentials
 enterprise_search_args = load_credentials("credentials.twitter_keys.yaml",
                                           yaml_key="search_tweets_enterprise",
                                           env_overwrite=False)
-
 
 search_query = "Superbowl"
 num_results = 500
@@ -28,8 +21,7 @@ fromdate = "200801010000"
 todate = "201710070000"
 
 # initialize temporary data structures to hold information
-data_collection = [[num_results-1], [num_results-1], [num_results-1], [num_results-1]]
-
+data_collection = [[num_results - 1], [num_results - 1], [num_results - 1], [num_results - 1]]
 
 rule = gen_rule_payload(search_query, from_date=fromdate, to_date=todate, results_per_call=num_results)
 print(rule)
@@ -39,6 +31,14 @@ tweets = collect_results(rule, max_results=num_results, result_stream_args=enter
 #  iterate through all collected tweet objects
 
 for tweet in tweets[0:num_results]:
-    session.execute(query, (tweet.all_text, tweet.created_at_datetime, tweet.generator.get("name"), tweet.geo_coordinates))
+    query = "INSERT INTO disasteranalytics.tweets (id, time, source, txt, coordinates) VALUES ({}, $${}$$, $${}$$, $${}$$, $${}$$)".format(
+        uuid.uuid4(),
+        str(tweet.created_at_datetime),
+        str(tweet.generator.get("name")),
+        str(tweet.all_text),
+        str(tweet.geo_coordinates)
+    )
+    session.execute(query)
+
     print(tweet.all_text, '\n', "Time: ", tweet.created_at_datetime, '\n', "Source:", tweet.generator.get("name"), '\n',
           "geo coordinates: ", tweet.geo_coordinates, '\n')
