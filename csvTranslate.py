c1cdf0a3194780a3925c4fc6f85ccb4a0fea9e0d
TaylorJesse <taylor.jesse@colorado.edu>
2018-04-19 19:01:56 -0600

    Changes to translate script

--- csvTranslate.py
+++ csvTranslate.py
@@ -6,10 +6,13 @@ with open('testFinalData.csv', 'w', newline='') as csvfile:
 	writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 	writer.writeheader()
 	
+	#mapTest --> CHANGE ME
 	with open('mapTest.txt') as oldFile:
 		#content = oldFile.readlines()
 		for line in oldFile:
 			line=line.strip('\n')
+			line=line.strip('(')
+			line=line.strip(')')
 			lat,Long,count=line.split(',')
 			writer.writerow({'latitude': lat, 'longitude': Long, 'count':count})
 
